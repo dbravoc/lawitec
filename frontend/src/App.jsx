cf8b1f2d7b748af18dbaa3524c6de2e2cdbc05b8
David Bravo Cisterna <bravocisterna@gmail.com>
2024-04-04 15:59:18 -0300

    tailwind andando

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-4xl text-center text-green-300'>
+      2020
+    </div>
+
+  )
+}
+
+export default App
